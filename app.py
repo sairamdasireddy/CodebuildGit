e717ae5bc675149c136a3412e1b9f5fe4e3f01a1
Sairam <sairamdasireddy444@gmail.com>
2023-10-06 15:03:06 +0530

    from the codebuild

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app=Flask(__name__)
 @app.route('/')
 def Sample():
-    return "This file from Git "
+    return "This file from Git !!!"
 if __name__=='__main__':
     app.run(  )
